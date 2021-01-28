164f218c43982faaf80741caedf95396c0d2df93
alamin <alamin@gmail.com>
2021-01-28 17:00:39 +0600

    updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoConnect = require("./mongo");
+const mongoConnect = require("./mongoose");
 
 const app = express();
 
--- mongo.js
+++ mongo.js
@@ -1,42 +1,42 @@
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
 
-const url =
-  "mongodb+srv://practice:practice@cluster0.b6wib.mongodb.net/Products?retryWrites=true&w=majority";
+// const url =
+//   "mongodb+srv://practice:practice@cluster0.b6wib.mongodb.net/Products?retryWrites=true&w=majority";
 
-const createProduct = async (req, res, next) => {
-  const newProduct = {
-    name: req.body.name,
-    price: req.body.price,
-  };
-  const client = new MongoClient(url);
+// const createProduct = async (req, res, next) => {
+//   const newProduct = {
+//     name: req.body.name,
+//     price: req.body.price,
+//   };
+//   const client = new MongoClient(url);
 
-  try {
-    await client.connect();
+//   try {
+//     await client.connect();
 
-    const db = client.db();
-    db.collection("products_demo").insertOne(newProduct);
-  } catch (error) {
-    return res.json({ message: "could not store data." });
-  }
+//     const db = client.db();
+//     db.collection("products_demo").insertOne(newProduct);
+//   } catch (error) {
+//     return res.json({ message: "could not store data." });
+//   }
 
-  client.close();
-  res.json(newProduct);
-};
+//   client.close();
+//   res.json(newProduct);
+// };
 
-const getProducts = async (req, res, next) => {
-  const client = new MongoClient(url);
+// const getProducts = async (req, res, next) => {
+//   const client = new MongoClient(url);
 
-  let products_demo1;
-  try {
-    await client.connect();
-    const db = client.db();
-    products_demo1 = await db.collection("products_demo").find().toArray();
-  } catch (error) {
-    return res.json({ message: "could not retrieve products." });
-  }
-  client.close();
-  res.json(products_demo1);
-};
+//   let products_demo1;
+//   try {
+//     await client.connect();
+//     const db = client.db();
+//     products_demo1 = await db.collection("products_demo").find().toArray();
+//   } catch (error) {
+//     return res.json({ message: "could not retrieve products." });
+//   }
+//   client.close();
+//   res.json(products_demo1);
+// };
 
-exports.createProduct = createProduct;
-exports.getProducts = getProducts;
+// exports.createProduct = createProduct;
+// exports.getProducts = getProducts;
